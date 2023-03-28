b006ac1f0967c1dfa605057d9d1c47b1dfa4dedb
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 21:04:29 +0800

    added logo for branding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import UserInput from "../components/UserInput.svelte";
   import BottomBar from "../components/BottomBar.svelte";
+  import Logo from "../components/Logo.svelte";
 </script>
 
 <main>
+  <Logo />
   <UserInput />
   <BottomBar />
 </main>
