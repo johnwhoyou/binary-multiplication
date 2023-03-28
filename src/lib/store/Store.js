593d1a4737b6ad2bcb23d8319c978773bafd5aed
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 13:44:44 +0800

    added central store

---
+++ src/lib/store/Store.js
@@ -0,0 +1,6 @@
+import { writable } from 'svelte/store';
+
+export const decMultiplicand = writable('');
+export const binMultiplicand = writable('');
+export const decMultiplier = writable('');
+export const binMultiplier = writable('');
