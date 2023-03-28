2012a4bf4550c060defc6a25c3327fd052c7abb0
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 21:02:02 +0800

    add website branding

---
+++ src/components/Logo.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { fly } from 'svelte/transition';
+    import { onMount } from 'svelte';
+  
+    let visible = false;
+  
+    onMount(() => {
+      visible = true;
+    });
+</script>
+  
+{#if visible}
+    <img src="https://i.imgur.com/0QOMof6.png" alt="download" style="pointer-events: none;" transition:fly="{{ y: 200, duration: 2000 }}" />
+{/if}  
