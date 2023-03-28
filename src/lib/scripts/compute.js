300b8ac22c1f02ae5793e66d1cd62b2c5e62189d
Ced <79917173+Revkxd@users.noreply.github.com>
2023-03-28 16:25:40 +0800

    Adjusted pencil and paper implementation

--- src/lib/scripts/compute.js
+++ src/lib/scripts/compute.js
@@ -2,20 +2,18 @@ function binaryAddition(a,b){
     let result = "", carry = 0
   
     while(a || b || carry){
-      let sum = +a.slice(-1) + +b.slice(-1) + carry // get last digit from each number and sum 
-  
-      if( sum > 1 ){  
-        result = sum%2 + result
-        carry = 1
-      }
-      else{
-        result = sum + result
-        carry = 0
-      }
-      
-      // trim last digit (110 -> 11)
-      a = a.slice(0, -1)
-      b = b.slice(0, -1)
+        let sum = +a.slice(-1) + +b.slice(-1) + carry // get last digit from each number and sum 
+        if( sum > 1 ){  
+            result = sum%2 + result
+            carry = 1
+        }
+        else{
+            result = sum + result
+            carry = 0
+        }
+        // trim last digit (110 -> 11)
+        a = a.slice(0, -1)
+        b = b.slice(0, -1)
     }
     
     return result
@@ -24,8 +22,8 @@ function binaryAddition(a,b){
 /**
  * This function implements the pencil and paper approach to binary multiplication.
  * 
- * @param {string} multiplicand 
- * @param {string} multiplier 
+ * @param {string} multiplicand binary string
+ * @param {string} multiplier binary string
  * @returns {Object} with params answer in binary, steps in array of strings
  */
 export function pencil_and_paper(multiplicand, multiplier){
@@ -38,9 +36,10 @@ export function pencil_and_paper(multiplicand, multiplier){
             let val = multiplicand[0].repeat(ans_length - multiplicand.length) + multiplicand + "0".repeat(multiplier.length - 1 - i);
             val = val.slice(val.length - ans_length);
             ans = binaryAddition(ans, val);
+            val = val.slice(0, val.length - (multiplier.length - 1 - i))
             steps.push(val);
         }else{
-            let val = "0".repeat(ans_length);
+            let val = "0".repeat(ans_length - (multiplier.length - 1 - i));
             ans = binaryAddition(ans, val);
             steps.push(val);
         }
@@ -85,3 +84,4 @@ export function sequential_circuit(multiplicand, multiplier){
 // Test Cases
 console.log('Test 1:\n', pencil_and_paper("01010", "101"));
 console.log('Test 2:\n', pencil_and_paper("01111", "0111"));
+console.log('Test 3:\n', pencil_and_paper("011", "01001"));
