c46c9451527f1bce869f829309f4fbc968e8556d
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 11:27:49 +0800

    Add isValidDec and isValidBin functions to inputUtils.js to check if decimal and binary operands are valid and supported by the calculator.

---
+++ src/lib/scripts/inputUtils.js
@@ -0,0 +1,62 @@
+/**
+ * Checks if the decimal operand is valid and supported by the calculator.
+ *
+ * Note that this methods recognizes '+' or '-' as valid strings. However, they are properly handled
+ * in the method canMultiply().
+ *
+ * @param {string} value Decimal operand.
+ * @returns {boolean} `true` if the decimal operand is valid and supported by the calculator (or if `value`
+ * is either a positive or a negative sign); `false`, otherwise.
+ */
+export function isValidDec(value) {
+	/* Regex that zero or one positive or negative sign, followed by zero or more digits */
+	const pattern = /^(-|\+)?(\d+)?$/;
+	if (!pattern.test(value)) {
+		return false;
+	}
+
+	/* Check if the value is within the range of 16-bit signed integers. */
+	if (parseInt(value) > Math.pow(2, 16 - 1) - 1) {
+		return false;
+	}
+
+	if (parseInt(value) < -1 * Math.pow(2, 16 - 1)) {
+		return false;
+	}
+
+	/* Check if value is null */
+	if (value.length == 0) {
+		return false;
+	}
+
+	/* No error found */
+	return true;
+}
+
+/**
+ * Checks if the binary operand is valid and supported by the calculator.
+ *
+ * @param {string} value Binary operand.
+ * @returns {boolean} `true` if the binary operand is valid and supported by the calculator; `false`,
+ * otherwise.
+ */
+export function isValidBin(value) {
+	/* Regex that only accepts zero or more 0s or 1s */
+	const pattern = /^[0-1]*$/;
+	if (!pattern.test(value)) {
+		return false;
+	}
+
+	/* Check if the value is within the range of supported signed integers. */
+	if (value.length > 16) {
+		return false;
+	}
+
+	/* Check if value is null */
+	if (value.length == 0) {
+		return false;
+	}
+
+	/* No error found */
+	return true;
+}
