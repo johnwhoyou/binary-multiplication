855bb21b8dbcc840d4c4a4c7465e7e048da52648
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 21:01:06 +0800

    changes to BottomBar

--- src/components/BottomBar.svelte
+++ src/components/BottomBar.svelte
@@ -1,17 +1,21 @@
-<div class="navbar bg-base-100 fixed bottom-0 w-full z-10">
+<div class="navbar bg-base-300 fixed bottom-0 w-full z-10">
     <div class="navbar-start">
-        <button class="btn">Binary Multiplication</button>
+        <button class="btn btn-ghost gap-2 btn-xs sm:btn-sm md:btn-md">
+            <img src="https://cdn-icons-png.flaticon.com/512/54/54529.png" class="h-6 w-6" alt="download" />
+            <p class="hidden sm:inline">Reset</p>
+        </button>
     </div>
     <div class="navbar-center">
-    <div class="btn-group">
-        <button class="btn">«</button>
-        <button class="btn">Step 22</button>
-        <button class="btn">»</button>
-      </div>
+        <div class="btn-group grid grid-cols-3">
+            <button class="btn btn-outline btn-xs sm:btn-sm md:btn-md">Previous</button>
+            <button class="btn btn-outline btn-xs sm:btn-sm md:btn-md">Step 22</button>
+            <button class="btn btn-outline btn-xs sm:btn-sm md:btn-md">Next</button>
+        </div>
     </div>
     <div class="navbar-end">
-        <button class="btn btn-ghost btn-circle">
-            <img src="https://www.svgrepo.com/show/499788/download.svg" class="h-5 w-5" alt="download" />
-        </button>          
+        <button class="btn btn-ghost gap-2 btn-xs sm:btn-sm md:btn-md">
+            <img src="https://cdn-icons-png.flaticon.com/512/724/724933.png" class="h-6 w-6" alt="download" />
+            <p class="hidden sm:inline">Download</p>
+        </button>   
     </div>
 </div>
