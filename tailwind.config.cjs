bdfba976de06a687241dda72224a889ba5037c80
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 21:00:47 +0800

    Update Tailwind config to include custom theme colors.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      }
-    },
-  },
   daisyui: {
-    themes: ["light", "dark", "bumblebee"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#0e9ad1", 
+          "secondary": "#36d1a0",     
+          "accent": "#f3a1fc",   
+          "neutral": "#1A1C2D",    
+          "base-100": "#E4ECF1",     
+          "info": "#4B8FD2",  
+          "success": "#25D0AE",   
+          "warning": "#E5B815",    
+          "error": "#FB326B",
+        },
+      },
+    ],
   },
-  plugins: [require('@tailwindcss/forms'), require('daisyui')],
+  plugins: [require('daisyui')],
 }
