914a050398f70a26d35573b4944b4ad305d98933
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-28 11:27:16 +0800

    Add BottomBar, step count, and download button.

---
+++ src/components/BottomBar.svelte
@@ -0,0 +1,17 @@
+<div class="navbar bg-base-100 fixed bottom-0 w-full z-10">
+    <div class="navbar-start">
+        <button class="btn">Binary Multiplication</button>
+    </div>
+    <div class="navbar-center">
+    <div class="btn-group">
+        <button class="btn">«</button>
+        <button class="btn">Step 22</button>
+        <button class="btn">»</button>
+      </div>
+    </div>
+    <div class="navbar-end">
+        <button class="btn btn-ghost btn-circle">
+            <img src="https://www.svgrepo.com/show/499788/download.svg" class="h-5 w-5" alt="download" />
+        </button>          
+    </div>
+</div>
