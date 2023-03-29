d461987d0e4169a05bf9615f39b82d3903c2da55
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-29 21:57:53 +0800

    Add AnswerSection and associated logic to page route

--- src/lib/store/Store.js
+++ src/lib/store/Store.js
@@ -1,7 +1,12 @@
 import { writable } from 'svelte/store';
 
-export const multiplyDisabled = writable(true);
 export const decMultiplicand = writable('');
 export const binMultiplicand = writable('');
 export const decMultiplier = writable('');
 export const binMultiplier = writable('');
+
+export const multiplyDisabled = writable(true);
+export const stepCounter = writable(-1);
+export const solvingMode = writable('Solving Mode');
+export const algorithm = writable('Algorithm');
+export const answers = writable({});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script>
+  import { answers } from '../lib/store/Store';
   import UserInput from "../components/UserInput.svelte";
   import BottomBar from "../components/BottomBar.svelte";
   import Logo from "../components/Logo.svelte";
+  import AnswerSection from "../components/AnswerSection.svelte";
 </script>
+
+<style>
+  main {
+      padding-bottom: 4rem; 
+  }
+</style>
 
 <main>
   <Logo />
   <UserInput />
-  <BottomBar />
+  {#if $answers.steps}
+    <AnswerSection />
+  {/if}
 </main>
+<BottomBar />
