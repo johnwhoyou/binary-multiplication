7bb4fc291cdc686a100e72f9c530119888bded9d
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-29 21:57:40 +0800

    Commit message: Add MathJax component and refactor Booths Algorithm for improved code readability

--- src/lib/scripts/compute.js
+++ src/lib/scripts/compute.js
@@ -106,13 +106,13 @@ function convert_multiplier_to_booths(multiplier){
  * @returns {Object} with params answer in binary, steps in array of strings
  */
 export function booths_algorithm(multiplicand, multiplier){
-    multiplicand = multiplicand.length >= multiplier.length ? multiplicand : multiplicand[0].repeat(multiplier.length - multiplicand.length) + multiplicand;
-    multiplier = multiplier.length >= multiplicand.length ? multiplier : multiplier[0].repeat(multiplicand.length - multiplier.length) + multiplier;
+    //multiplicand = multiplicand.length >= multiplier.length ? multiplicand : multiplicand[0].repeat(multiplier.length - multiplicand.length) + multiplicand;
+    //multiplier = multiplier.length >= multiplicand.length ? multiplier : multiplier[0].repeat(multiplicand.length - multiplier.length) + multiplier;
     const ans_length = multiplicand.length + multiplier.length;
     let ans = "";
     let steps = [];
     multiplier = convert_multiplier_to_booths(multiplier);
-    for(let i = 0; i < multiplier.length; i++){
+    for(let i = multiplier.length - 1; i >= 0; i--){
         if(multiplier[i] == "+1"){
             let val = multiplicand[0].repeat(ans_length - multiplicand.length) + multiplicand + "0".repeat(multiplier.length - 1 - i);
             val = val.slice(val.length - ans_length);
