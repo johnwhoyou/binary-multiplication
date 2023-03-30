8d3e304eb3b0c70005e90cf1c7d667898b8c2437
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-03-30 18:46:06 +0800

    added conditional rendering logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
-  import { answers } from '../lib/store/Store';
+  import { penPaper, booths, boothsExt, seqCircuit } from '../lib/store/Store';
   import UserInput from "../components/UserInput.svelte";
   import BottomBar from "../components/BottomBar.svelte";
   import Logo from "../components/Logo.svelte";
   import AnswerSection from "../components/AnswerSection.svelte";
+
+  let stepsExist = {};
+  $: {
+    // If penPaper is not empty it means all algorithms have a value
+    stepsExist = Object.keys($penPaper).length === 0 ? false : true;
+  }
 </script>
 
 <style>
@@ -15,7 +21,7 @@
 <main>
   <Logo />
   <UserInput />
-  {#if $answers.steps}
+  {#if stepsExist}
     <AnswerSection />
   {/if}
 </main>
