b8aa061851ea0899ba6ad490b54127b4b1991282
zen-young <renzcruz50@gmail.com>
2023-04-11 00:48:30 +0800

    implemented latex generator

--- src/components/MathJax.svelte
+++ src/components/MathJax.svelte
@@ -1,22 +1,24 @@
 <script>
-	import { createEventDispatcher, onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
 	export let math;
-	let mathContent ='';
-	const dispatch = createEventDispatcher();
-	
+
 	onMount(() => {
-	    let script = document.createElement('script');
-        script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
-        document.head.append(script);
-            
-        script.onload = () => {
-        MathJax = {
-            tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
-            svg: {fontCache: 'global'}
-        };
-            };
-            console.log("Here")
+		let script = document.createElement('script');
+		script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+		document.head.append(script);
+
+		script.onload = () => {
+			MathJax = {
+				tex: {
+					inlineMath: [
+						['$', '$'],
+						['\\(', '\\)']
+					]
+				},
+				svg: { fontCache: 'global' }
+			};
+		};
 	});
 </script>
 
