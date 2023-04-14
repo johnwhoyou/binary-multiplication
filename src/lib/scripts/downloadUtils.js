728994a0911b9cc6bf25cef632c515c7508395f0
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-04-14 22:56:50 +0800

    download generator done

--- src/lib/scripts/downloadUtils.js
+++ src/lib/scripts/downloadUtils.js
@@ -13,36 +13,61 @@ export function generateTextContent(answers, algorithm, decMultiplicand, decMult
 
     content += `Multiplicand\n`;
     content += `\tDecimal:\t${decMultiplicand}\n`;
-    content += `\tBinary:\t${answers.multiplicand}\n\n`;
+    content += `\tBinary:\t\t${answers.multiplicand}\n\n`;
     content += `Multiplier\n`;
     content += `\tDecimal:\t${decMultiplier}\n`;
-    content += `\tBinary:\t${answers.multiplier}\n\n`;
+    content += `\tBinary:\t\t${answers.multiplier}\n\n`;
 
     content += `----------------------------------------------------------------\n\n`;
    
-    content += `\t`;
-    content += " ".repeat(answers.steps.length)
-    content += `${answers.multiplicand}\n`;
-
-    content += ` x\t`;
-    content += " ".repeat(answers.steps.length)
-    if (algorithm === 'Pencil and Paper') {
-        content += `${answers.multiplier}\n`;
-    } else if (algorithm === "Booth's") {
-        content += `${answers.booth_multiplier.join(' ')}\n`;
+    if (algorithm === "Booth's") {
+        content += `\t`;
+        content += " ".repeat(answers.multiplicand.length)
+        content += `${answers.multiplicand}\n`;
+    } else if (algorithm === 'Sequential Circuit') {
+        content += `Initial:\n`;
+        content += `\tM=${answers.initial.M}\t-M=${answers.initial['-M']}\n`;
+        content += `\tA=${answers.initial.A}\tQ=${answers.initial.Q}\tQ-1=${answers.initial['Q-1']}\n\n`;
+    } else {
+        content += `\t`;
+		content += ' '.repeat(answers.multiplicand.length);
+        content += `${answers.multiplicand}\n`;
     }
 
-    content += `----------------------------------------\n`;
+    if (algorithm !== 'Sequential Circuit') {
+        content += ` x\t`;
+    }
+    if (algorithm === 'Pencil and Paper') {
+        content += ' '.repeat(answers.multiplicand.length);
+        content += `${answers.multiplier}\n`;
+    } else if (algorithm === "Booth's" ) {
+        content += `${answers.booth_multiplier.join(' ')}\n`;
+    } else if (algorithm === "Extended Booth's") {
+        content += ' '.repeat(answers.steps.length);
+        content += `\t${answers.booth_multiplier.join(' ')}\n`;
+    }
 
-    for (let i = 0; i < answers.steps.length; i++) {
-        if (i === answers.steps.length-1) {
-            content += ` +\t${answers.steps[i].value}\n`;
-        } else {
-            content += `\t${answers.steps[i].value}\n`;
+     if (algorithm !== 'Sequential Circuit') {
+        content += `----------------------------------------------------------\n`;
+    }
+
+    if (algorithm === "Sequential Circuit") {
+        for (let i = 0; i < answers.steps.length; i++) {
+            content += `Loop ${i+1}:\n`;
+            content += `\tA=${answers.steps[i].A}\tQ=${answers.steps[i].Q}\tQ-1=${answers.steps[i]['Q-1']}\n\n`;
+        }
+    } else {
+        for (let i = 0; i < answers.steps.length; i++) {
+            if (i === answers.steps.length - 1) {
+                content += ` +\t${answers.steps[i].value}\n`;
+            } else {
+                content += `\t${answers.steps[i].value}\n`;
+            }
         }
     }
-    content += `----------------------------------------\n`;
-    content += `\t${answers.answer}`;
+
+    content += `----------------------------------------------------------\n`;
+    content += `Result:\t${answers.answer}`;
 
     return content;
 }
